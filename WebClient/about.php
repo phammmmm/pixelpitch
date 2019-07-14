4026183f745936d2a9efe6c0e97560d559c12f35
Michael <xuanduoc.mai@gmail.com>
2019-07-14 16:39:44 +0000

    update about us

--- WebClient/about.php
+++ WebClient/about.php
@@ -1,5 +1,112 @@
 <?php require 'header.php'; ?>
-<h1>About</h1>
-  <p>This is a simple ecommerce website selling men T-shirt.</p>
-  <p>App Version: 1.0</p>
+<header id="gtco-header" class="gtco-cover gtco-cover-xs" role="banner">
+  <div class="gtco-container">
+    <div class="row">
+      <div class="col-md-12 col-md-offset-0 text-left">
+        <div class="display-t">
+          <div class="display-tc">
+            <div class="row">
+              <div class="col-md-8">
+                <h1 class="no-margin">About Us</h1>
+                <p>Singapore based and owned, we are committed to fast turnaround times without compromising on quality.
+                  Sweatshop free and child labor free!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<div class="gtco-section">
+  <div class="gtco-container">
+    <div class="row">
+      <div class="col-md-12 col-md-offset-0 gtco-heading text-center">
+        <h2>Who We Are</h2>
+        <p>Pixel Pitch started with the concept to provide high quality material for custom t-shirts as seamlessly as possible.
+          Along the way, we also got rid of the usual minimum quantity requirement and limited color restrictions that is still the norm in 2019.
+          We made designing custom t-shirts so easy and unique. Our slogan is “Everyone is super model” so we create the huge range of t-shirts
+          that represent your characteristics and bring the best experiences to our customers.
+          Singapore based and owned, we are committed to fast turnaround times without compromising on quality.
+          Sweatshop free and child labor free!</p><br /><br /><br />
+
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <div class="gtco-testimony gtco-left">
+              <h3>Peggy Tio</h3>
+              <p class="role">Founder</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-sm-6">
+            <div class="gtco-testimony gtco-left">
+              <h3>Pham</h3>
+              <p class="role">Co-Founder</p><br />
+            </div>
+          </div>
+
+          <div class="col-md-6 col-sm-6">
+            <div class="gtco-testimony gtco-left">
+              <h3>Henry Jonny</h3>
+              <p class="role">Business Director</p><br />
+            </div>
+          </div>
+
+          <div class="col-md-6 col-sm-6">
+            <div class="gtco-testimony gtco-left">
+              <h3>Melisa Marasigan</h3>
+              <p class="role">Marketing Director</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-sm-6">
+            <div class="gtco-testimony gtco-left">
+              <h3>Sharon Uy</h3>
+              <p class="role">Account &amp; Admin Director</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-sm-6">
+            <div class="gtco-testimony gtco-left">
+              <h3>Helene Huang</h3>
+              <p class="role">Creative Director</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="gtco-section gtco-testimonial gtco-gray">
+  <div class="gtco-container">
+    <div class="row">
+      <div class="col-md-12 col-md-offset-0 gtco-heading text-center">
+        <h2>What We Do</h2>
+        <p>To help accentuate the distinction of your personal style, we create designs imbued with interesting details, and produce these designs in limited quantities.
+          We promise to deliver high quality, comfortable fitting, unique products that empower you to feel nothing less than your best.
+          Sustainability is close to our heart. We produce environmentally friendly T-shirts in order to reduce waste and we recycle whenever possible.
+          At our store, no T-shirt Is an ordinary shirt.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="gtco-section">
+  <div class="gtco-container">
+    <div class="row">
+      <div class="col-md-12 col-md-offset-0 gtco-heading text-center">
+        <h2>Shop For A Good Cause</h2>
+        <p> We are friends of charitable endeavours, and we donate to acknowledged charitable organizations.
+          This is how we can harness the potential we have as a continuously growing company to support values and things close to our heart.
+          And we never condition our donations on purchases made with our company.
+          We do, however, grant concessionary terms to recognized organizations using print-on-demand apparel to point to social injustices.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 <?php require 'footer.php'; ?>
