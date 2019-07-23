5d1a23f410f29722e7c585fa0864a8f76a1acbba
Michael <xuanduoc.mai@gmail.com>
2019-07-23 13:13:29 +0000

    fix url path

--- Server/get-product-info.php
+++ Server/get-product-info.php
@@ -1,19 +1,20 @@
 <?php
-require_once '../Server/ProductController.php';
-$db_handle = new ProductController();
-
-$product = $db_handle->getProductById($_GET["product_id"]);
-if (! empty($product)) {
-?>  
-      <div class="preview-image">
-        <img src="<?php echo "../".$product["product_image"] ; ?>" />
-      </div>
-      <div class="product-info">
-          <div class="product-title"><?php echo $product["product_title"] ; ?></div>
-          <div class="product-category"><?php echo $product["category"] ; ?></div>
-          <div class="product-desc"><?php echo $product["product_description"] ; ?></div>
-          <div>$<?php echo $product["product_price"] ; ?></div>
-      </div>      
-<?php
+if(!empty($_GET["keyword"])) {
+  require_once '../Server/ProductController.php';
+  $db_handle = new ProductController();
+  $product = $db_handle->getProductById($_GET["product_id"]);
+  if (! empty($product)) {
+  ?>  
+        <div class="preview-image">
+          <img src="<?php echo "../".$product["product_image"] ; ?>" />
+        </div>
+        <div class="product-info">
+            <div class="product-title"><?php echo $product["product_title"] ; ?></div>
+            <div class="product-category"><?php echo $product["category"] ; ?></div>
+            <div class="product-desc"><?php echo $product["product_description"] ; ?></div>
+            <div>$<?php echo $product["product_price"] ; ?></div>
+        </div>      
+  <?php
+  }
 }
 ?>
