54a4bb3a6c6497494d0bce5dfab2540e13132d20
Michael <xuanduoc.mai@gmail.com>
2019-07-23 12:59:05 +0000

    fix url path

--- WebClient/js/product.js
+++ WebClient/js/product.js
@@ -5,15 +5,25 @@ $(".quick_look").on("click", function() {
       height: 'auto',
       width:'70%'
     };
-  $('#product_preview').load('http://'+window.location.hostname+':'+window.location.port+'/pixelpitch/Server/get-product-info.php?product_id='+product_id).dialog(options).dialog('open');
+  var url = window.location.href;
+  var pos = url.lastIndexOf("/");
+  url = url.substring(0,pos)
+  $('#product_preview').load(url+'/../Server/get-product-info.php?product_id='+product_id).dialog(options).dialog('open');
 });
 $(".cat_button").on("click", function() {
   var cat_id = $(this).attr("data-id");
-  $('#product-list').load('http://'+window.location.hostname+':'+window.location.port+'/pixelpitch/Server/category.php?cat_id='+cat_id);
+  var url = window.location.href;
+  var pos = url.lastIndexOf("/");
+  url = url.substring(0,pos)
+  $('#product-list').load(url+'/../Server/category.php?cat_id='+cat_id);
 });
 
 function searchTee(){
-  $('#product-list').load('http://'+window.location.hostname+':'+window.location.port+'/pixelpitch/Server/searchProducts.php?keyword='+$('#keyword').val());
+  var url = window.location.href;
+  var pos = url.lastIndexOf("/");
+  url = url.substring(0,pos)
+
+  $('#product-list').load(url+'/../Server/searchProducts.php?keyword='+$('#keyword').val());
 }
 
 
